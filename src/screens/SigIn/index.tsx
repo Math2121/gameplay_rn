d96ea694a2634a45b5e56a2f89bc7675c513242c
Math2121 <matheusdepaula527@gmail.com>
2021-06-21 11:35:34 -0300

    add ScrollView Component

--- src/screens/SigIn/index.tsx
+++ src/screens/SigIn/index.tsx
@@ -1,35 +1,37 @@
 import React from "react";
-import { View, Text, Image, StatusBar } from "react-native";
+import { View, Text, Image, StatusBar, ScrollView } from "react-native";
 import { styles } from "./style";
-import  {ButtonIcon}  from "../../components/ButtonIcon";
+import { ButtonIcon } from "../../components/ButtonIcon";
 import IllustrationImg from "../../assets/illustration.png";
 export function SigIn() {
   return (
-    <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Image
-        source={IllustrationImg}
-        style={styles.image}
-        resizeMode={"stretch"}
-      />
+    <ScrollView>
+      <View style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Image
+          source={IllustrationImg}
+          style={styles.image}
+          resizeMode={"stretch"}
+        />
 
-      <View style={styles.content}>
-        <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
-        </Text>
-        <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
-          favoritos com seus amigos
-        </Text>
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Organize {`\n`}
+            suas jogatinas {`\n`}
+            facilmente
+          </Text>
+          <Text style={styles.subtitle}>
+            Crie grupos para jogar seus games {`\n`}
+            favoritos com seus amigos
+          </Text>
 
-        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+          <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
